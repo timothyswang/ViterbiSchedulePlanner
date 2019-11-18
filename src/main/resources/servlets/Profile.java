9b7833b682dbc186db9248fc3442ae2777944c00
niconeven <nneven@usc.edu>
2019-11-17 18:31:09 -0800

    Update Profile.java

--- src/main/resources/servlets/Profile.java
+++ src/main/resources/servlets/Profile.java
@@ -6,12 +6,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.MongoClientSettings;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Servlet implementation class Profile
  */
 public class Profile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	ConnectionString connString = new ConnectionString(
+		"mongodb+srv://password:username@cluster0-v2kcb.gcp.mongodb.net/test?retryWrites=true&w=majority"
+	);
+	MongoClientSettings settings = MongoClientSettings.builder()
+	    .applyConnectionString(connString)
+	    .retryWrites(true)
+	    .build();
+	MongoClient mongoClient = MongoClients.create(settings);
+	MongoDatabase database = mongoClient.getDatabase("ViterbiSchedule");
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,7 +55,8 @@ public class Profile extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		MongoCollection<Document> collection = database.getCollection("Users");
 	}
 
 	/**
