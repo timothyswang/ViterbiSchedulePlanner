ab0d1f52d56d5cb26e5dd905014365daa921001f
niconeven <nneven@usc.edu>
2019-11-19 12:01:12 -0800

    Update Login.java

--- servlets/Login.java
+++ servlets/Login.java
@@ -1,10 +1,28 @@
 
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.mongodb.*;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import com.mongodb.client.model.Sorts;
+import java.util.Arrays;
+import org.bson.Document;
 
 /**
  * Servlet implementation class Login
@@ -25,7 +43,28 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		ConnectionString connString = new ConnectionString(
+			"mongodb+srv://password:username@cluster0-v2kcb.gcp.mongodb.net/test?retryWrites=true&w=majority"
+		);
+		MongoClientSettings settings = MongoClientSettings.builder()
+		    .applyConnectionString(connString)
+		    .retryWrites(true)
+		    .build();
+		MongoClient mongoClient = MongoClients.create(settings);
+		MongoDatabase database = mongoClient.getDatabase("ViterbiSchedule");
+		MongoCollection<Document> collection = database.getCollection("Users");
+		String username = request.getParameter("username");
+		Document user = collection.find(eq("_id", username)).first();
+		if (user != null) {
+			session.setAttribute("user", user.toJson());
+			RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/schedule.jsp");
+	        dispatch.forward(request, response);
+		} else {
+			RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/login.jsp");
+	        dispatch.forward(request, response);
+		}
+		
 	}
 
 	/**
