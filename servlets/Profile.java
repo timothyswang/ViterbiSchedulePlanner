cdeafb5c299b224dbd10c9e20489ad5012eb41b5
niconeven <nneven@usc.edu>
2019-11-18 19:39:58 -0800

    Update Profile.java

--- servlets/Profile.java
+++ servlets/Profile.java
@@ -1,10 +1,31 @@
 
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.*;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Filters;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import com.mongodb.client.model.Sorts;
+import java.util.Arrays;
+import org.bson.Document;
 
 /**
  * Servlet implementation class Profile
@@ -25,6 +46,19 @@ public class Profile extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		// ...
+		ConnectionString connString = new ConnectionString(
+				"mongodb+srv://password:username@cluster0-v2kcb.gcp.mongodb.net/test?retryWrites=true&w=majority"
+		);
+		MongoClientSettings settings = MongoClientSettings.builder()
+		    .applyConnectionString(connString)
+		    .retryWrites(true)
+		    .build();
+		MongoClient mongoClient = MongoClients.create(settings);
+		MongoDatabase database = mongoClient.getDatabase("ViterbiSchedule");
+		MongoCollection<Document> collection = database.getCollection("Users");
+		collection.find().forEach(printBlock); // PRINTS EACH USER JSON TO CONSOLE
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -35,5 +69,11 @@ public class Profile extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	Block<Document> printBlock = new Block<Document>() {
+	       public void apply(final Document document) {
+	           System.out.println(document.toJson());
+	       }
+	};
 
 }
